b2a7922bc36d6807e8bbeba070c2dd4aee81ce2b
RoyDong <roy.dongwei@gmail.com>
2013-12-19 06:08:35 +0800

    tb

--- controller/main.go
+++ controller/main.go
@@ -1,8 +1,9 @@
 package controller
 
 import (
-    "time"
+    "log"
     "github.com/roydong/potato"
+    ws "code.google.com/p/go.net/websocket"
 )
 
 type Main struct {
@@ -11,6 +12,18 @@ type Main struct {
 
 func (c *Main) Index() {
     c.RenderJson("111")
-    
-    time.Sleep(time.Second * 10)
+}
+
+func (c *Main) Ws() {
+    conn := c.Request.WSConn
+    var a string
+
+    for {
+        if err := ws.Message.Receive(conn, &a); err != nil {
+            log.Println(err)
+        }
+
+        log.Println(a)
+        ws.Message.Send(conn, a)
+    }
 }
