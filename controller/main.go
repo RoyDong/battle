6bebdb4d8344e61a99c5d480a304df73092cd8e0
RoyDong <roy.dongwei@gmail.com>
2013-12-31 00:24:49 +0800

    aa

--- controller/main.go
+++ controller/main.go
@@ -12,12 +12,12 @@ type Main struct {
 }
 
 type Topic struct {
-    Id int64 `id`
-    Title string `title`
-    Content string `content`
-    State int `state`
-    CreatedAt time.Time `created_at`
-    UpdatedAt time.Time `updated_at`
+    Id int64 `column:"id"`
+    Title string `column:"title"`
+    Content string `column:"content"`
+    State int `column:"state"`
+    CreatedAt time.Time `column:"created_at"`
+    UpdatedAt time.Time `column:"updated_at"`
 }
 
 type topicModel struct {
@@ -28,12 +28,20 @@ var TopicModel = &topicModel{orm.NewModel("topic", new(Topic))}
 
 func (c *Main) Index() {
     stmt := orm.NewStmt()
-    rows := stmt.Select("t.id, t.title, t.content, t.state, t.created_at, t.updated_at").From("Topic", "t").Where("t.id = :id").Query(map[string]interface{} {"id": 1})
+    stmt.Select("t.id, t.title, t.content, t.state, t.created_at, t.updated_at").
+        From("Topic", "t").
+        Where("t.id = :id").
+        Query(map[string]interface{} {"id": 1})
+
+    log.Println("--")
+        /*
     var t Topic
 
     e := rows.Scan(&t)
 
     log.Println(t, e)
+    
+    */
     c.Response.Write([]byte(stmt.String()))
 }
 
