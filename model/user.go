f78666356f479919f223c8ed652e87281e4f05e2
RoyDong <roy.dongwei@gmail.com>
2014-01-09 01:21:39 +0800

    aa

--- model/user.go
+++ model/user.go
@@ -11,20 +11,16 @@ import (
 )
 
 type User struct {
-    id int64
-    passwd, salt string
-
-    Name, Email string
-    CreatedAt, UpdatedAt time.Time
+    Id int64 `column:"id"`
+    Passwd string `column:"passwd"`
+    Salt string `column:"salt"`
+    Name string `column:"name"`
+    Email string `column:"email"`
+    CreatedAt time.Time `column:"created_at"`
+    UpdatedAt time.Time `column:"updated_at"`
 }
 
-func (u *User) Id() int64 {
-    return u.id
-}
 
-/**
- * set a hash password
- */
 func (u *User) SetPasswd(passwd string) {
     rnd := make([]byte, 32)
     if _,e := io.ReadFull(rand.Reader, rnd); e != nil {
@@ -36,61 +32,52 @@ func (u *User) SetPasswd(passwd string) {
         panic("could not hash salt")
     }
 
-    u.salt = hex.EncodeToString(hash.Sum(nil))
-    u.passwd = UserModel.HashPasswd(passwd, u.salt)
+    u.Salt = hex.EncodeToString(hash.Sum(nil))
+    u.Passwd = UserModel.HashPasswd(passwd, u.Salt)
 }
 
 func (u *User) CheckPasswd(passwd string) bool {
-    return UserModel.HashPasswd(passwd, u.salt) == u.passwd
+    return UserModel.HashPasswd(passwd, u.Salt) == u.Passwd
 }
 
-
-type userModel struct {Model}
-
-var UserModel = &userModel{
-    Model{"user", []string{
-            "id","email","name","passwd","salt","created_at","updated_at"}}
+type userModel struct {
+    *orm.Model
 }
+
+var UserModel = &userModel{orm.NewModel("user", new(User))}
 
 func (m *userModel) User(id int64) *User {
-    row := m.FindOne(map[string]interface{"id": id}, nil)
-    return m.loadData(row)
-}
+    var u *User
+    rows, e := orm.NewStmt().Select("u.*").From("User", "u").
+            Where(fmt.Sprintf("u.id = %d", id)).Query(nil)
 
-func (m *userModel) UserByEmail(email string) *User {
-    row := m.FindOne(map[string]interface{"email": email}, nil)
-    return m.loadData(row)
-}
-
-func (m *userModel) loadData(row potato.Scanner) *User {
-    u := new(User)
-    var ct, ut int64
-    if e := row.Scan(&u.id, &u.Email, &u.Name, &u.passwd, &u.salt, &ct, &ut); e != nil {
-        return nil
+    if e == nil && rows.Next() {
+        rows.ScanEntity(&u)
     }
 
-    u.CreatedAt = time.Unix(0, ct)
-    u.UpdatedAt = time.Unix(0, ut)
     return u
 }
 
-func (m *userModel) Save(u *User) bool {
-    data := map[string]interface{} {
-        "email": u.Email,
-        "name": u.Name,
-        "passwd": u.passwd,
-        "salt": u.salt,
-        "created_at": u.CreatedAt.UnixNano(),
-        "updated_at": u.UpdatedAt.UnixNano(),
+func (m *userModel) UserByEmail(email string) *User {
+    var u *User
+    rows, e := orm.NewStmt().Select("u.*").From("User", "u").
+            Where("u.email = :e").Query(map[string]interface{}{"e": email})
+
+    if e == nil && rows.Next() {
+        rows.ScanEntity(&u)
     }
 
-    if u.Id() > 0 {
-        return m.Update(data, map[string]interface{}{"id": u.id}) > 0
-    }
-
-    u.id = m.Insert(data)
-    return u.id > 0
+    return u
 }
+
+
+func (m *userModel) Exists(email string) bool {
+    n,_ := orm.NewStmt().Count("User", "u").
+            Where(fmt.Sprintf("u.email = '%s'", email)).Exec(nil)
+
+    return n > 0
+}
+
 
 func (m *userModel) HashPasswd(passwd string, salt string) string {
     hash := sha512.New()
